5a8670976ae8008a9fcfe702ac8fc30ea67d5e73
mysecondpride <englishbybudi@gmail.com>
2025-05-02 13:55:29 +0700

    bismillah again

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,25 @@
 const mongoose = require("mongoose");
 // require("dotenv").config();
 
+// const connectDB = async () => {
+//   try {
+//     mongoose.set("strictQuery", false);
+//     const conn = await mongoose.connect(process.env.MONGODB_URI);
+//     console.log(`Database connected: ${connection.host}`);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// server/config/db.js (example)
+
 const connectDB = async () => {
   try {
-    mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
-    console.log(`Database connected: ${connection.host}`);
-  } catch (error) {
-    console.log(error);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+    process.exit(1);
   }
 };
-
 module.exports = connectDB;
